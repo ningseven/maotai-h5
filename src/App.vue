4aef0f29d6fcb3a39d8767abf51ed64c523918b1
宁、 <921050772@qq.com>
2020-07-06 10:09:26 +0800

    项目初始化git

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  // mounted(){
+  //    this.$nextTick(function() {
+  //       // 适配不同屏幕，改变根font-size值
+  //         if (document.documentElement.clientWidth <= document.documentElement.clientHeight) {
+  //             document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px';
+  //         } else {
+  //             document.documentElement.style.fontSize = document.documentElement.clientWidth / 13.34 + 'px';
+  //         }
+  //     })     
+  // }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" class="testRem" src="../assets/logo.png" />
+    <span class="testSpan">哈哈哈</span>
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style lang="scss" >
+.testRem {
+  width: 2rem;
+}
+.testSpan {
+  font-size: 0.8rem;
+}
+</style>
