c79e7eaacd4600c954a7a92aa7d82ce6cec27952
宁、 <921050772@qq.com>
2020-07-09 18:56:43 +0800

    同步代码

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <!-- <keep-alive>
       <router-link to="/"></router-link> 
-      </keep-alive> -->
+      </keep-alive>-->
       <!-- <router-link to="/about">About</router-link> -->
     </div>
     <router-view />
@@ -19,19 +19,37 @@ export default {
   //         } else {
   //             document.documentElement.style.fontSize = document.documentElement.clientWidth / 13.34 + 'px';
   //         }
-  //     })     
+  //     })
   // }
-}
+};
 </script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
-
+.flex {
+  display: flex;
+}
+h2 {
+  margin-top: 0 !important;
+  margin-bottom: 0 !important;
+}
+.mt-2{
+  margin-top: .2rem;
+}
+.colorGray{
+  color: #908B8B;
+}
+.fs-normal{
+  font-size: .34rem;
+}
+.aJump{
+    color: #2c3e50!important
+}
 #nav {
   // padding: 30px;
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -9,6 +9,7 @@
           <div class="iconBox qiye">
             <i class="iconfont icon-qiye"></i>
           </div>
+          <span>企业官网</span>
         </div>
       </van-col>
       <van-col span="6">
@@ -16,25 +17,73 @@
           <div class="iconBox shangcheng">
             <i class="iconfont icon-shangcheng"></i>
           </div>
+          <span>线上商城</span>
         </div>
       </van-col>
       <van-col span="6">
-        <div class="navCard">
-          <div class="iconBox weixin">
-            <i class="iconfont icon-weixin"></i>
+        <a
+          href="https://mp.weixin.qq.com/mp/profile_ext?action=home&amp;__biz=MjM5MjY0NjY4MA==&amp;scene=126&amp;bizpsid=0#wechat_redirect"
+          class="aJump"
+        >
+          <div class="navCard">
+            <div class="iconBox weixin">
+              <i class="iconfont icon-weixin"></i>
+            </div>
+            <span>微信公众号</span>
           </div>
-        </div>
+        </a>
       </van-col>
       <van-col span="6">
-        <div class="navCard">
+        <div class="navCard" @click="telShow=true">
           <div class="iconBox dianhua">
             <i class="iconfont icon-dianhua"></i>
           </div>
+          <span>联系我们</span>
         </div>
       </van-col>
     </van-row>
+    <div class="cutOffRule"></div>
 
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div class="bottom">
+      <van-row>
+        <van-col span="1" offset="1">
+          <span class="verticalBar"></span>
+        </van-col>
+        <van-col span="20">
+          <h2 class="bottomTitle">贵州茅台集团白金酒公司</h2>
+        </van-col>
+      </van-row>
+
+      <van-row class="mt-2 fs-normal">
+        <van-col span="2" offset="3" class="colorGray">品类</van-col>
+        <van-col span="10" offset="3">白金</van-col>
+      </van-row>
+      <van-row class="mt-2 fs-normal">
+        <van-col span="2" offset="3" class="colorGray">类型</van-col>
+        <van-col span="10" offset="3">酒类</van-col>
+      </van-row>
+      <van-row class="mt-2 fs-normal">
+        <van-col span="2" offset="3" class="colorGray">厂商</van-col>
+        <van-col span="14" offset="3">贵州茅台酒厂(集团)白金酒有限责任公司</van-col>
+      </van-row>
+
+      <div class="bottomImg">
+        <img
+          style="width:100%"
+          src="http://supermanage.gzzlfw.com/Content/update/201906/10/201906101413579356.jpg"
+          alt
+        />
+      </div>
+    </div>
+
+    <van-action-sheet
+      v-model="telShow"
+      :actions="actions"
+      cancel-text="取消"
+      description="全国咨询热线"
+      close-on-click-action
+      @select="callUp"
+    />
   </div>
 </template>
 
@@ -48,9 +97,21 @@ export default {
     // HelloWorld
   },
   data() {
-    return {};
+    return {
+      telShow: false,
+      actions: [{ name: "400-622-9988", color: "#2065DB" }]
+    };
   },
-  methods: {}
+  methods: {
+    callUp(val) {
+      console.log(val);
+      // document.createElement()
+      var a = document.createElement("a");
+      a.setAttribute("href", "tel:400-622-9988");
+      document.body.appendChild(a);
+      a.click();
+    }
+  }
 };
 </script>
 <style lang="scss" >
@@ -63,35 +124,58 @@ export default {
   }
   .iconNav {
     // .van-col {
-    //   text-align: center;
+    text-align: center;
     // }
+    margin-top: 0.3rem;
     .navCard {
       margin: 0 auto;
-      width: 1.1rem;
+      // width: 1.1rem;
+      width: 100%;
+      span {
+        display: block;
+        margin-top: 0.3rem;
+      }
     }
     .iconBox {
       width: 0.9rem;
-      height: 0.9rem;
+      height: 0.85rem;
       padding-top: 0.1rem;
       margin: 0 auto;
       border-radius: 50%;
     }
-    .qiye{
-        background-color: #63BEEE;
+    .qiye {
+      background-color: #63beee;
     }
-    .shangcheng{
-        background-color: #E56C71;
+    .shangcheng {
+      background-color: #e56c71;
     }
-     .weixin{
-        background-color: #3FC7A3;
+    .weixin {
+      background-color: #3fc7a3;
     }
-     .dianhua{
-        background-color: #E99E22;
+    .dianhua {
+      background-color: #e99e22;
     }
   }
-
-  .testSpan {
-    font-size: 0.8rem;
+  .cutOffRule {
+    width: 98%;
+    margin: 0.4rem auto;
+    background-color: #e7eaed;
+    height: 0.2rem;
+  }
+  .bottom {
+    .verticalBar {
+      display: block;
+      width: 0.12rem;
+      height: 0.7rem;
+      background-color: #3fc7a3;
+    }
+    .bottomTitle {
+      display: inline;
+      font-weight: 400;
+    }
+    .bottomImg {
+      margin-bottom: 1.5rem;
+    }
   }
 }
 </style>
